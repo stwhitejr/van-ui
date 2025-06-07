96cae45c0a6d633dd1fef4065a79411ba808e2aa
Steve White <stwhitejr@gmail.com>
2025-06-07 16:52:39 -0700

    import

--- backend/hardware/level_sensor.py
+++ backend/hardware/level_sensor.py
@@ -12,33 +12,31 @@ def checkLevel():
     from mpu6050 import mpu6050
     import math
 
-    sensor = mpu6050(0x68, 1)
+    sensor = mpu6050(0x68)
     accel_data = sensor.get_accel_data()
 
     ax = accel_data["x"]
     ay = accel_data["y"]
     az = accel_data["z"]
 
-    return {"x": ax}
+    pitch = math.degrees(math.atan2(ax, math.sqrt(ay**2 + az**2)))
+    roll = math.degrees(math.atan2(ay, math.sqrt(ax**2 + az**2)))
 
-    # pitch = math.degrees(math.atan2(ax, math.sqrt(ay**2 + az**2)))
-    # roll = math.degrees(math.atan2(ay, math.sqrt(ax**2 + az**2)))
+    # Assume 0° = 100% level, and ±10° = 0% level
+    max_angle = 10
 
-    # # Assume 0° = 100% level, and ±10° = 0% level
-    # max_angle = 10
+    # Compute total deviation
+    deviation = math.sqrt(pitch**2 + roll**2)
+    level_percent = max(0, 100 - (deviation / max_angle) * 100)
+    level_percent = round(level_percent, 2)
 
-    # # Compute total deviation
-    # deviation = math.sqrt(pitch**2 + roll**2)
-    # level_percent = max(0, 100 - (deviation / max_angle) * 100)
-    # level_percent = round(level_percent, 2)
-
-    # return {
-    #     "pitch": abs(round(pitch, 2)),
-    #     "roll": abs(round(roll, 2)),
-    #     "level_percent": level_percent,
-    #     "pitch_rating": getRating(pitch),
-    #     "roll_rating": getRating(roll),
-    # }
+    return {
+        "pitch": abs(round(pitch, 2)),
+        "roll": abs(round(roll, 2)),
+        "level_percent": level_percent,
+        "pitch_rating": getRating(pitch),
+        "roll_rating": getRating(roll),
+    }
 
 
 def checkLevelMock():
