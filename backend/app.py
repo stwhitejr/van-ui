d99ec084ca8b8d0739d4df45476d0f1540c7b00b
Steve White <stwhitejr@gmail.com>
2025-06-07 16:42:02 -0700

    temp remove

--- backend/app.py
+++ backend/app.py
@@ -3,40 +3,42 @@ from hardware import (
     LevelSensor,
     InverterToggle,
     Smartshunt,
-    LEDController,
-    VoiceRecognition,
+    # LEDController,
+    # VoiceRecognition,
 )
 import time
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 from threading import Thread
 
-load_dotenv()
+# load_dotenv()
 
-leds = LEDController()
+# leds = LEDController()
 
 app = Flask(__name__, static_folder="../dist")
 
-INVERTER_ON_COLOR = "(248, 232, 58)"
+INVERTER_ON_COLOR = "248, 232, 58"
 
 
 # API
 @app.route("/inverter/toggle", methods=["POST"])
 def toggleInverter():
     data = InverterToggle()
-    if data.on:
-        leds.turn_on()
-        leds.set_color(INVERTER_ON_COLOR)
-    else:
-        leds.turn_off()
+    # if data.on:
+    # leds.turn_on()
+    # leds.set_color(INVERTER_ON_COLOR)
+    # else:
+    # leds.turn_off()
 
     return jsonify(data)
 
 
 @app.route("/smartshunt/data", methods=["GET"])
 def smartshunData():
-    data = Smartshunt()
+    return True
+    # data = Smartshunt()
 
-    return jsonify(data)
+    # return jsonify(data)
 
 
 @app.route("/level_sensor/data", methods=["GET"])
@@ -45,53 +47,53 @@ def levelsensorData():
     return jsonify(data)
 
 
-@app.route("/leds/configure", methods=["POST"])
-def configureLeds():
-    """
-    Expected request payload:
-    {
-      "on": true,
-      "brightness": 70,
-      "color": "#ffcc00",
-      "sleep": 5,
-      "preset": "rainbow"
-    }
-    """
-    data = request.json
+# @app.route("/leds/configure", methods=["POST"])
+# def configureLeds():
+#     """
+#     Expected request payload:
+#     {
+#       "on": true,
+#       "brightness": 70,
+#       "color": "#ffcc00",
+#       "sleep": 5,
+#       "preset": "rainbow"
+#     }
+#     """
+#     data = request.json
 
-    if data is None:
-        return jsonify({"error": "Invalid request"}), 400
+#     if data is None:
+#         return jsonify({"error": "Invalid request"}), 400
 
-    on = data.get("on")
-    brightness = data.get("brightness")
-    color = data.get("color")
-    sleep_duration = data.get("sleep", 0)
+#     on = data.get("on")
+#     brightness = data.get("brightness")
+#     color = data.get("color")
+#     sleep_duration = data.get("sleep", 0)
 
-    if brightness is not None:
-        leds.set_brightness(brightness)
+#     if brightness is not None:
+#         leds.set_brightness(brightness)
 
-    if color:
-        try:
-            leds.set_color(color)
-        except ValueError:
-            return jsonify({"error": "Invalid color format"}), 400
+#     if color:
+#         try:
+#             leds.set_color(color)
+#         except ValueError:
+#             return jsonify({"error": "Invalid color format"}), 400
 
-    if data.preset:
-        try:
-            leds.run_preset(data.preset)
-        except ValueError as e:
-            return jsonify({"error": str(e)}), 400
+#     if data.preset:
+#         try:
+#             leds.run_preset(data.preset)
+#         except ValueError as e:
+#             return jsonify({"error": str(e)}), 400
 
-    if on is True:
-        leds.turn_on()
-    elif on is False:
-        leds.turn_off()
+#     if on is True:
+#         leds.turn_on()
+#     elif on is False:
+#         leds.turn_off()
 
-    if sleep_duration:
-        time.sleep(sleep_duration)
-        leds.turn_off()
+#     if sleep_duration:
+#         time.sleep(sleep_duration)
+#         leds.turn_off()
 
-    return jsonify(leds.status())
+#     return jsonify(leds.status())
 
 
 # Frontend
@@ -106,7 +108,7 @@ def static_proxy(path):
 
 
 if __name__ == "__main__":
-    if VoiceRecognition:
-        Thread(target=VoiceRecognition, daemon=True).start()
+    # if VoiceRecognition:
+    #     Thread(target=VoiceRecognition, daemon=True).start()
 
     app.run(host="0.0.0.0", port=5000, debug=True)
