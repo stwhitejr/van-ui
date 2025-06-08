77996993e085675f27004ffc87716496b3328805
Steve White <stwhitejr@gmail.com>
2025-06-07 17:17:10 -0700

    fix

--- backend/hardware/inverter.py
+++ backend/hardware/inverter.py
@@ -1,23 +1,17 @@
 def toggleInverter():
     from gpiozero import LED
-    from time import sleep
 
     relay = LED(26)
-    relay.on()
-    print("Relay ON (should click)")
-    sleep(10)
-    relay.off()
-    print("Relay OFF (should click again)")
 
-    # try:
-    #     if relay.is_active:
-    #         relay.off()
-    #         return {"on": False, "success": True}
-    #     else:
-    #         relay.on()
-    #         return {"on": True, "success": True}
-    # except Exception as e:
-    #     return {"on": relay.is_active, "success": False, "error": str(e)}
+    try:
+        if relay.value == 1:
+            relay.off()
+            return {"on": False, "success": True}
+        else:
+            relay.on()
+            return {"on": True, "success": True}
+    except Exception as e:
+        return {"on": bool(relay.value), "success": False, "error": str(e)}
 
 
 def toggleInverterMock():
