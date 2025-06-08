ce0ae342a877448f3b5b633596de74345f1080ab
Steve White <stwhitejr@gmail.com>
2025-06-07 17:24:25 -0700

    decreate time

--- backend/hardware/inverter.py
+++ backend/hardware/inverter.py
@@ -5,16 +5,14 @@ relay = LED(26)
 
 
 def toggleInverter():
-    print("relay", relay)
-
     try:
         if relay.is_active:
             relay.off()
-            sleep(2)
+            sleep(0.05)
             return {"on": False, "success": True}
         else:
             relay.on()
-            sleep(2)
+            sleep(0.05)
             return {"on": True, "success": True}
     except Exception as e:
         return {"on": relay.is_active, "success": False, "error": str(e)}
